008ded70cbef8b90d09bde7d83cf2c4e93ab868c
Tse018 <thanushan.s@hotmail.com>
2021-11-09 13:23:16 +0100

    Mobile + Desktop

---
+++ js/mobile.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('mobile-menu');
+const navbarMenu = document.querySelector('.navbar__menu');
+
+
+/***** Model ******/
+const navbar = {
+   showMenu: false,
+   notShowMenu: true
+};
+
+/******* View *********/
+function updateView() {
+   if(navbar.showMenu ===  true) {
+      navbarMenu.classList.remove('active')
+   } else {
+      navbarMenu.classList.add('active')
+   }
+};
+
+/****** Controller *******/
+function clickedOnNavbar() {
+   navbar.showMenu = !navbar.showMenu;
+   updateView();
+}
+
+/**** addEventListener *********/
+button.addEventListener('click', clickedOnNavbar)
+
+
+
+
