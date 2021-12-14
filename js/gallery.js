32e55b4d1e24bc6b1a0b78e2c8392f248f87ef2e
tse018 <60438717+tse018@users.noreply.github.com>
2021-12-14 19:42:35 +0100

    desktop versions is complete

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,10 @@
-/*
-   TODO
-   - add images
-	- function rendering images
- */
-
 /************** MODEL *****************/
 const galleryImages = {
    images: [
       {
          title: "bilde1",
          caption: "Bilde1",
-         img: "assets/bilde1.png",
+         img: "assets/bilde1.jpg",
       },
       {
          title: "bilde2",
@@ -30,12 +24,12 @@ const galleryImages = {
       {
          title: "bilde5",
          caption: "Bilde5",
-         img: "assets/400/bilde5.jpg",
+         img: "assets/bilde5.jpg",
       },
       {
          title: "bilde6",
          caption: "Bilde6",
-         img: "assetsbilde6.jpg",
+         img: "assets/bilde6.jpg",
       },
       {
          title: "bilde7",
@@ -56,11 +50,6 @@ const galleryImages = {
          title: "bilde10",
          caption: "Bilde10",
          img: "assets/bilde10.jpg",
-      },
-      {
-         title: "bilde11",
-         caption: "Bilde11",
-         img: "assets/bilde11.jpg",
       },
       {
          title: "bilde12",
@@ -88,11 +77,6 @@ const galleryImages = {
          img: "assets/bilde16.jpg",
       },
       {
-         title: "bilde17",
-         caption: "Bilde17",
-         img: "assets/bilde17.jpg",
-      },
-      {
          title: "bilde18",
          caption: "Bilde18",
          img: "assets/bilde18.jpg",
@@ -116,24 +100,15 @@ function renderImages() {
 
       figcaptionElement.innerText = image.caption;
 
+      figureElement.className = "work__figure";
+      figcaptionElement.className = "work__caption";
+      imgElement.className = "work__image";
+
       figureElement.appendChild(imgElement);
       figureElement.appendChild(figcaptionElement);
-      figureElement.className = "work__galleryshow--images";
 
       images.appendChild(figureElement);
    });
 }
 
 renderImages();
-
-/***** Horizontal Auto scrolling *****************/
-const galleryShowcase = document.querySelector(".work__galleryshow");
-const galleryScrollWidth = galleryShowcase.scrollWidth;
-
-window.addEventListener("load", () => {
-   this.setInterval(() => {
-      if (galleryShowcase.scrollLeft !== galleryScrollWidth) {
-         galleryShowcase.scrollTo(galleryShowcase.scrollLeft + 1, 0);
-      }
-   }, 15);
-});
